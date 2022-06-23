8d075ce932653e5e7038015aec2a3485240d690f
satyam1209 <satyamsingh12092000@gmail.com>
2022-06-23 09:28:16 +0530

    Updates

---
+++ precache-manifest.c6388f9c8b821f00620049037c51e93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a8dee317358ed27e865",
+    "url": "/TextUtils-3/static/css/main.d987ce55.chunk.css"
+  },
+  {
+    "revision": "9a8dee317358ed27e865",
+    "url": "/TextUtils-3/static/js/main.9a8dee31.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/TextUtils-3/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "67a84f94a3104a39a4e6",
+    "url": "/TextUtils-3/static/js/2.67a84f94.chunk.js"
+  },
+  {
+    "revision": "32065c79cb76a774dbc1",
+    "url": "/TextUtils-3/static/js/runtime~main.32065c79.js"
+  },
+  {
+    "revision": "2ff7eb7a1a4c65a4fd14db13c0148919",
+    "url": "/TextUtils-3/index.html"
+  }
+];
